b9c12044d7180adddce1b6aa7312676918458369
olatundemarvelousanthony <olatundemarvelousanthony@gmail.com>
2025-11-07 15:01:45 +0000

    Fix: Added cookieParser package.

--- olamarvel/week-7/backend/src/index.ts
+++ olamarvel/week-7/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import { connectDB } from "./config/db";
 import activityRoutes from "./Routes/activities";
@@ -35,6 +36,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/activities", activityRoutes);
 app.use("/api/appointments", appointmentRoutes);
